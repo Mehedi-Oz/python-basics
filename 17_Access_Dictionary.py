65c8533e5c78644499a11c86c1d8243b7d2c48e1
Muhammad Mehedi Hasan <mehedihasan199908@gmail.com>
2025-06-14 23:57:26 +0600

    dictionary

--- 17_Access_Dictionary.py
+++ 17_Access_Dictionary.py
@@ -28,13 +28,55 @@ x = thisdict.items()
 print(x)
 
 
-
-car = {
-"brand": "Ford",
-"model": "Mustang",
-"year": 1964
-}
+car = {"brand": "Ford", "model": "Mustang", "year": 1964}
 x = car.items()
-print(x) #before the change
+print(x)  # before the change
 car["year"] = 2020
-print(x) #after the change
+print(x)  # after the change
+
+
+newDict = {
+    "key1": "value1",
+    "key2": "value2",
+    "key3": "value3",
+}
+print(newDict)
+
+print(newDict.get("key3"))
+
+print(newDict.keys())
+print(newDict.items())
+print(newDict.values())
+
+
+newDict["key4"] = "value4"
+print(newDict)
+
+
+newDict = {
+    "key1": "value1",
+    "key2": "value2",
+    "key3": "value3",
+}
+
+for _ in newDict.values():
+    print(f"Current key: {_}")
+    if "value3" in _:
+        print("gotcha!")
+    else:
+        print("sorry boss!")
+
+
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+thisdict.update({"year": 2020})
+print(thisdict)
+
+
+newDict = {
+    "key1": "value1",
+    "key2": "value2",
+    "key3": "value3",
+}
+# newDict.pop("key2")
+thisdict.popitem()
+print(newDict)
